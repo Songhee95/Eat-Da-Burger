5df9aa775190162b7a731f20fa3002fee3c0ea74
songhee <thdgml5537@gmail.com>
2020-11-29 01:15:15 -0500

    config directory setup

---
+++ config/orm.js
@@ -0,0 +1,15 @@
+const connection = require("./connection");
+
+const orm = {
+    selectAll : function(){
+        //function goes here
+    },
+    insertOne : function(){
+        //function goes here
+    },
+    updateOne : function(){
+        //function goes here
+    }
+}
+
+module.exports = orm;
