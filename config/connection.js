b55b408f861020564654683b93e014cfa259ee9e
songhee <thdgml5537@gmail.com>
2020-12-01 00:49:02 -0500

    heroku deployment error

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,8 @@ const connection = mysql.createConnection({
 })
 connection.connect(function(err){
     if(err){
-        console.error("error connecting: "+ err.stack)
+        console.error("error connecting: "+ err.stack);
+        return;
     }
     console.log("connected as id "+ connection.threadId)
 })
