4eb02a8e7291b0c1edbdfd5fd16565922348a013
songhee <thdgml5537@gmail.com>
2020-12-01 10:51:46 -0500

    heroku error : server port change

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 const app = express();
+
+app.set("port", (process.env.POR || 8080));
 
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
@@ -15,6 +17,6 @@ var routes = require('./controllers/burgers_controller.js');
 
 app.use(routes);
 
-app.listen(PORT, function(){
+app.listen(app.get('port'), function(){
     console.log("App now listening at localhost: " + PORT);
 })
